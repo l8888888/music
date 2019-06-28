e043eff9e46cdc82d75b0d02243bef16a8672745
wayne-music <291203069@qq.com>
2019-06-29 00:09:23 +0800

    music 20190629

---
+++ vue.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: { // 路径名替换，@已经内置配置好为src
+        'components': '@/components',
+        'assets': '@/assets',
+        'common': '@/common',
+        'api': '@/api',
+        'base': '@/base'
+      }
+    }
+  },
+  // baseUrl:'/',
+  devServer: {
+    open: true, // 自动打开浏览器页面
+    port: 8081, // 端口
+    proxy: {
+      '/api': {
+        target: 'http://35.236.155.170:3000/',
+        pathRewrite: {
+          '^/api': '/'
+        }
+      }
+    }
+  }
+}
